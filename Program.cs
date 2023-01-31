af42bddaf342ad9c795bbc23035095ab9a60feb9
AlekseyBelyaev2606 <partchoice44@gmail.com>
2023-01-31 23:04:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите  число");
+int N = int.Parse(Console.ReadLine());
+string Value = Convert.ToString(N);
+if (Value.Length < 3){
+    Console.WriteLine("Такого числа нет");
+}
+else{
+    Console.WriteLine(Value[2]);
+}
